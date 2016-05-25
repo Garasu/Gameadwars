89fef24fcfda08144f90d5674dae4176e2593f1e
Stephan Mueller <s.m@tuta.io>
2016-05-25 15:11:14 +0200

    updated compile script

--- compile.sh
+++ compile.sh
@@ -2,9 +2,12 @@
 
 javac src/game/Main.java &&
 
-jar -cfm Gameadwars.jar manifest.mf src/Spielfeld/*.class src/controller/*.class src/game/*.class src/window/*.class &&
+jar -cfm Gameadwars.jar manifest.mf src/Spielfeld/*.class src/game/*.class src/window/*.class &&
+# src/controller/*.class
 
 rm src/Spielfeld/*.class
 rm src/game/*.class
 rm src/window/*.class
 # rm src/controller/*.class
+
+java -jar Gameadwars.jar
